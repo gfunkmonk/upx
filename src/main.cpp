0453b96d1dad1699f4d63235ed92127b671f8845
Matt Rinsch <gfunkmonk@mailbox.org>
2026-03-18 12:40:41 -0400

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -470,8 +470,8 @@ static int do_option(int optc, const char *arg) {
             e_method(M_ZSTD, opt->level);
         break;
 #endif
-#if WITH_BZIP2
-    case 729:
+#if WITH_	BZIP2
+    case 726:
         opt->method_bzip2_seen = true;
         if (!set_method(M_BZIP2, -1))
             e_method(M_BZIP2, opt->level);
@@ -913,7 +913,7 @@ int main_get_options(int argc, char **argv) {
         {"prefer-nrv", 0x10, N, 723},
         {"prefer-ucl", 0x10, N, 724},
         {"zstd", 0x10, N, 725},    // --zstd
-        {"bzip2", 0x10, N, 729},    // --bzip2
+        {"bzip2", 0x10, N, 726},    // --bzip2
         {"all-filters", 0x10, N, 523},
         {"all-methods", 0x10, N, 524},
         {"exact", 0x10, N, 525},  // user requires byte-identical decompression
