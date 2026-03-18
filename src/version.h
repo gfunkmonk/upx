7264fcf337a204cfd53e8f31eb79a9f9cd121acf
Matt Rinsch <gfunkmonk@mailbox.org>
2026-03-18 05:38:01 -0400

    version

--- src/version.h
+++ src/version.h
@@ -2,6 +2,6 @@
 #define UPX_VERSION_HEX      0x050103 /* 05.01.03 */
 #define UPX_VERSION_STRING   "5.1.3"
 #define UPX_VERSION_STRING4  "5.13"
-#define UPX_VERSION_DATE     "Mar 17th 2026"
-#define UPX_VERSION_DATE_ISO "2026-03-17"
+#define UPX_VERSION_DATE     "Mar 18th 2026"
+#define UPX_VERSION_DATE_ISO "2026-03-18"
 #define UPX_VERSION_YEAR     "2026"
